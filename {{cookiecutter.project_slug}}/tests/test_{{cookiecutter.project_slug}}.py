e10d40d9c0e69c2f35c5300513d5e81bbeb37a59
Sebastian Schmeier <s.schmeier@gmail.com>
2020-01-02 14:29:39 +1300

    Added wrong config to have a run fail.

--- {{cookiecutter.project_slug}}/tests/test_{{cookiecutter.project_slug}}.py
+++ {{cookiecutter.project_slug}}/tests/test_{{cookiecutter.project_slug}}.py
@@ -1,24 +1,21 @@
 import pytest
-from subprocess import call, Popen, PIPE
+from subprocess import call
 import os
+import yaml
 
 
 """
 test {{cookiecutter.project_slug}}
 
-this test will run {{cookiecutter.project_slug}} with the test
-config and params provided in the test dir.
+this test will test {{cookiecutter.project_slug}}.
 
 this test will also show how to run tests where
 failure is expected (i.e., checking that we handle
 invalid parameters).
-
-each test has a unittest TestCase defined.
-pytest will automatically find these tests.
 """
 
 
-class Test:
+class TestCLI:
     """
     simple {{cookiecutter.project_slug}} test class
 
@@ -29,55 +26,50 @@ class Test:
     """
 
     @classmethod
-    def setUpClass(self):
+    def setup_class(self):
+        """ setup any state specific to the execution of the given class (which
+        usually contains tests).
         """
-        set up a {{cookiecutter.project_slug}} workflow test.
 
-        we are using the existing tests/ dir
-        as our working dir, so no setup to do.
-
-        if we were expecting the user to provide
-        a Snakefile, this is where we would set
-        up a test Snakefile.
-        """
-        pass
-
-    def test_setup(self):
+    def testSetup(self):
         """
         test workflow
         """
         command = ["{{ cookiecutter.project_slug }}", "setup", "-n", "test"]
         pwd = os.path.abspath(os.path.dirname(__file__))
-        child = Popen(command, cwd=pwd, stdout=PIPE)
-        streamdata = child.communicate()[0]
-        rc = child.returncode
+        rc = call(command, cwd=pwd)
         assert rc == 0
 
-    def test_run(self):
+    @pytest.mark.parametrize(
+        "test_input_config,expected",
+        [("test/config.yaml", 0), ("config_wrong.yaml", 1)],
+    )
+    def test_run(self, test_input_config, expected):
         """
         test workflow
         """
         command_prefix = ["{{ cookiecutter.project_slug }}", "run"]
-
-        params = ["test/config.yaml"]
-
         pwd = os.path.abspath(os.path.dirname(__file__))
 
-        for param in params:
+        command = command_prefix + [test_input_config]
+        rc = call(command, cwd=pwd)
+        assert rc == expected
 
-            command = command_prefix + [param]
-
-            child = Popen(command, cwd=pwd, stdout=PIPE)
-            streamdata = child.communicate()[0]
-            rc = child.returncode
-            assert rc == 0
-
-            # clean up
-            call(["rm", "-rf", "analysis"])
+        # clean up run dat
+        # config files here specify a resultdir where the snakemake run results
+        # will be written to. Here we find it for each indifivual run and delete
+        # the directory after successful runs.
+        config_data = yaml.safe_load(open(os.path.join(pwd, test_input_config)))
+        print(config_data)
+        resultdir = config_data["resultdir"]
+        rc = call(["rm", "-rf", resultdir], cwd=pwd)
+        assert rc == 0
 
     @classmethod
-    def tearDownClass(self):
+    def teardown_class(self):
+        """ teardown any state that was previously setup with a call to
+        setup_class.
         """
-        clean up after the tests
-        """
-        pass
+        pwd = os.path.abspath(os.path.dirname(__file__))
+        rc = call(["rm", "-rf", "test"], cwd=pwd)
+        assert rc == 0
